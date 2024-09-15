3410e89e908a21a4f6633d7037de7c02723f5065
Jackkyjack <akeraanucha1@gmail.com>
2024-09-15 21:33:20 +0700

    Update tailwindcss to version 3.4.11

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 const Profile = () => {
   return (
     <div className="flex h-full overflow-y-hidden divide-x-2 pb-10 pt-16">
-      <div className='flex flex-col text-white w-1/5 items-center justify-between py-[5rem] '>
-        <p className=' '>John Figma</p>
-        <button>hee</button>
+      <div className='flex flex-col text-white w-1/5 items-center justify-between  '>
+        <div className='flex flex-col relative'>
+          <icon></icon>
+          <p className=' '>John Figma</p>
+        </div>
+          <button>hee</button>
       </div>
-      <div className='flex flex-col text-white w-4/5 items-center justify-between py-[5rem] bg-indigo-500 '>
+      <div className='flex flex-col text-white w-4/5 items-center justify-between bg-indigo-500 '>
         <p className=' '>John Figma</p>
         <button>hee</button>
       </div>
