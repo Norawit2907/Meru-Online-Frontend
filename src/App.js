a21781b9b487e888d135190f1888ba7016862dfb
Yotsanon Punthongpaun <Yotsanon2003@gmail.com>
2024-09-19 00:05:36 +0700

    feat main page

--- src/App.js
+++ src/App.js
@@ -4,24 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <p>test commit pap</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Title-header">
+        <div className="title">
+          <h1>จัดงานศพ</h1>
+          <h1>ให้กับคนที่คุณรัก</h1>
+        </div>
+        <h2>ค้นหาวัด สำหรับงานศพของคุณ</h2>
+      
+      
       </header>
+
+      
     </div>
   );
 }
-
 export default App;
+
