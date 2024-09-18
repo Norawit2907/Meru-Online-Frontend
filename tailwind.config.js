ed307bc9006b1c467a61db103f8b16f5c98123b6
Yotsanon Punthongpaun <Yotsanon2003@gmail.com>
2024-09-19 00:25:41 +0700

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
   theme: {
     extend: {
-      fontFamily: {
-        propmt: ["Prompt"],
+      animation: {
+        fadeIn: 'fadeIn 2s ease-out',
+        fadeInUp: 'fadeInUp 1.25s ease-in-out 0.25s 1',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translate3d(0, 100%, 0)' },
+          '100%': { opacity: '1', transform: 'translate3d(0, 0, 0)' },
+        },
       },
     },
   },
   plugins: [],
-};
+}
+
+
