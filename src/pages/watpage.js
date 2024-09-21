644c7d783c80b2a0f68197a012a8042496c9a02a
PonChuenjitsiri <111419266+PonChuenjitsiri@users.noreply.github.com>
2024-09-21 16:11:03 +0700

    Update react-router-dom to version 6.26.2

---
+++ src/pages/watpage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Watpage = () => {
+  return (
+    <div className="w-full h-full mx-[77px]">
+      <div className="grid grid-cols-6 h-[540px]">
+        <div className="row-span-4 bg-[#C2C6CC] w-full h-full">
+          <h1>Hi</h1>
+        </div>
+
+        <div className="row-span-2">
+          <h1>H12</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Watpage;
