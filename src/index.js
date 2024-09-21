6ec33586d3f9cf8b11a2722a02d0d7ea300a397e
PonChuenjitsiri <111419266+PonChuenjitsiri@users.noreply.github.com>
2024-09-21 16:18:55 +0700

    Update tailwindcss to version 3.4.12

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Watpage from './pages/watpage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/watpage" element={<Watpage />} />
-    </Routes>
-  </Router>
+  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
