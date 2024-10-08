2992beb5bac79a13d757a7bb17c8e1d409f0194a
Withwws <mondoi2003@gmail.com>
2024-10-08 23:15:00 +0700

    gg

--- src/App.js
+++ src/App.js
@@ -4,34 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <header className="Title-header">
         <div className="title">
           <h1>จัดงานศพ</h1>
           <h1>ให้กับคนที่คุณรัก</h1>
         </div>
         <h2>ค้นหาวัด สำหรับงานศพของคุณ</h2>
-      
-      
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-propmt font-bold underline text-black">Hello world!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
->>>>>>> 849e55fbc9f03b0e72a913c6d7347711b1695171
       </header>
-
-      
     </div>
   );
 }
