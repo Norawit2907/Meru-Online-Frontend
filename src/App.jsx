5c597976768ffaf2528126d96bcb61d4c956049e
Norawit2907 <norawit2907@gmail.com>
2024-10-13 21:03:37 +0700

    add watpage1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Result from "./pages/Result.jsx";
 import Homes from "./styles/Home.css";
 import Profile from "./pages/Profile.jsx";
 import Navbar from "./components/Navbar";
+import Watpage1 from './pages/Watpage1.jsx'
 import Watpage2 from './pages/Watpage2.jsx'
 import EditWat from "./pages/EditWat.jsx";
 
@@ -19,6 +20,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/result" element={<Result />} />
+            <Route path="/watpage1" element={<Watpage2/>} />
             <Route path="/watpage2" element={<Watpage2/>} />
             <Route path="/editwat" element={<EditWat></EditWat>}></Route>
           </Routes>
