e5f8bb9b2bb4fa27154936ed3d67195984da4618
Norawit2907 <norawit2907@gmail.com>
2024-11-01 02:41:47 +0700

    create addon

--- src/services/wat.js
+++ src/services/wat.js
@@ -57,6 +57,49 @@ export async function updateWat(wat_id, admin_id, admin_name, name, phoneNumber,
     }
 }
 
+export async function getAddonByWatId(wat_id){
+    try{   
+        const response = await axios.get(`${backendUrl}/addons/wat/${wat_id}`)
+        if(response.status == 200){
+            console.log("get", response.data);
+            
+            return response.data
+        }
+        else{
+            return false
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+export async function createAddon(wat_id, name, image, cost, catalog, description){
+    try{
+        
+        const response = await axios.post(`${backendUrl}/addons`,
+            {
+            wat_id,
+            name,
+            image,
+            cost,
+            catalog,
+            description
+            }
+        )
+        if(response.status == 201){
+            console.log("check",response.data);
+            return response.data
+        }
+        else{
+            return false
+        }
+    }
+    catch(err){
+        console.log(err.response.data.message);
+        
+    }
+}
 
 export async function getAddressByWatId(wat_id){
     try{
